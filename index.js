ec020ac6d2571aae941d7ec272529b38f1ee2643
thanormsaksudsee <s6506022420037@email.kmutnb.ac.th>
2023-10-16 23:35:19 +0700

    w

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const db = new sqlite3.Database('./Database/Animal.sqlite');
 
 app.use(express.json());
-app.use(express.static(__dirname + '/Myproject'));
+
 
 // สร้างตาราง Habitat of Animal
 db.run(`CREATE TABLE IF NOT EXISTS HabitatOfAnimal (
