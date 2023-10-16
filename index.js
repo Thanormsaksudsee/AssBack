1d5f5ad8b7d69ed9bc17854e735ceae62eb7870b
thanormsaksudsee <s6506022420037@email.kmutnb.ac.th>
2023-10-16 23:57:08 +0700

    a

--- index.js
+++ index.js
@@ -205,5 +205,7 @@ app.delete('/Habitat/:id', (req, res) => {
 });
 
 const port = process.env.PORT || 4000;
-app.listen(port, () => console.log(`Server is running on port http://localhost:${port}`));
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
 
